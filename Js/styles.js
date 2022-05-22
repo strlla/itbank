fb6c36c4fc141ad665d1858cbe12408174ab748e
TomasCalabria <totocalabria.tc@gmail.com>
2022-05-21 23:15:32 -0300

    agregando transicion de js

--- Js/styles.js
+++ Js/styles.js
@@ -1,6 +1,6 @@
 
 const loginBtn=document.querySelector('.loginBtn')
-const registerBtn =document.querySelector('#crearCuenta')    
+const registerBtn =document.querySelector('.signUpBtn')    
 const loginForm =document.querySelector('.loginForm')
 const registerForm=document.querySelector('.registerForm')
 const overlayRight=document.querySelector('.overlayRight')
@@ -10,7 +10,7 @@ document.addEventListener('click', e => {
     if (e.target === loginBtn || e.target===registerBtn) {
         registerForm.classList.toggle('active');
         overlayRight.classList.toggle('active')
-        loginForm.classList.toggle('active')
         overlayLeft.classList.toggle('active')
+        loginForm.classList.toggle('active')
     }
 });
