00dc0c510c09b24e9a8ec0f880ec09be71c36bff
TomasCalabria <totocalabria.tc@gmail.com>
2022-05-22 20:45:07 -0300

    modificando wallpaper login

--- Js/styles.js
+++ Js/styles.js
@@ -5,12 +5,14 @@ const loginForm =document.querySelector('.loginForm')
 const registerForm=document.querySelector('.registerForm')
 const overlayRight=document.querySelector('.overlayRight')
 const overlayLeft=document.querySelector('.overlayLeft')
-
+const formsContainer = document.querySelector('.formsContainer')
 document.addEventListener('click', e => {
     if (e.target === loginBtn || e.target===registerBtn) {
         registerForm.classList.toggle('active');
         overlayRight.classList.toggle('active')
         overlayLeft.classList.toggle('active')
         loginForm.classList.toggle('active')
+        formsContainer.classList.toggle('formsContainer2')
+ 
     }
 });
