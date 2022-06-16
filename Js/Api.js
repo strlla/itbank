70d0bb993fcf13085ced5bc98a3f692466fcc0e1
strlla <estrellaportocarrero@gmail.com>
2022-06-15 22:44:26 -0300

    Se guarda el contenedor en una variable

--- Js/Api.js
+++ Js/Api.js
@@ -1,34 +1,34 @@
-const datos=[]
-const escribirHtml=()=>{
-    let container =document.querySelector('.innerCotizaciones').innerHTML=datos.map(dato=>    `
-    <div class="cotizacion">
+const datos = [];
+
+const generarCotizaciones = () => {
+  let cotizacionesContainer = document.querySelector(".innerCotizaciones");
+  cotizacionesContainer.innerHTML = datos
+    .map(
+      (dato) => `
+    <div class="cotizacion card">
     
     <h4 class="titleCotizacion">Moneda: ${dato.casa.nombre}</h4>
     </br>
     <p class="cotizacionVenta">Precio Venta: ${dato.casa.venta}</p>
     <p class="cotizacionCompra">Precio Compra: ${dato.casa.venta}</p>
     </div>`
-    ).join('')
-    
-    }
-fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales',{
-    method: 'GET',
+    )
+    .join("");
+};
 
-}).catch(err=>console.log(err))
-
-.then((response) => response.json())
-    .then((responseJSON) => {
-        datos.push(...responseJSON)
-        datos.find(dato=>{
-            if(dato.casa.nombre==='Dolar Blue'){
-                console.log(`Moneda: Dolar Blue / Precio de Venta: ${dato.casa.venta} / Precio de Compra: ${dato.casa.compra}`)
-            }
-   
-    
-    
-    })
-escribirHtml()
+fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales", {
+  method: "GET",
 })
-
-
-
+  .then((response) => response.json())
+  .then((responseJSON) => {
+    datos.push(...responseJSON);
+    datos.find((dato) => {
+      if (dato.casa.nombre === "Dolar Blue") {
+        console.log(
+          `Moneda: Dolar Blue / Precio de Venta: ${dato.casa.venta} / Precio de Compra: ${dato.casa.compra}`
+        );
+      }
+    });
+    generarCotizaciones();
+  })
+  .catch((err) => console.log(err));
