413ce08f4356a217185620d837f8937ff13e97a3
TomasCalabria <totocalabria.tc@gmail.com>
2022-06-15 21:17:13 -0300

    imprimiendo en pantalla los resultados

--- Js/Api.js
+++ Js/Api.js
@@ -1,4 +1,16 @@
 const datos=[]
+const escribirHtml=()=>{
+    let container =document.querySelector('.innerCotizaciones').innerHTML=datos.map(dato=>    `
+    <div class="cotizacion">
+    
+    <h4 class="titleCotizacion">Moneda: ${dato.casa.nombre}</h4>
+    </br>
+    <p class="cotizacionVenta">Precio Venta: ${dato.casa.venta}</p>
+    <p class="cotizacionCompra">Precio Compra: ${dato.casa.venta}</p>
+    </div>`
+    ).join('')
+    
+    }
 fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales',{
     method: 'GET',
 
@@ -7,16 +19,16 @@ fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales',{
 .then((response) => response.json())
     .then((responseJSON) => {
         datos.push(...responseJSON)
-   
         datos.find(dato=>{
             if(dato.casa.nombre==='Dolar Blue'){
-                console.log(`Moneda: Dolar Blue
-----------------------
-Precio de Venta: ${dato.casa.venta} 
------------------------
-Precio de Compra: ${dato.casa.compra}`)
+                console.log(`Moneda: Dolar Blue / Precio de Venta: ${dato.casa.venta} / Precio de Compra: ${dato.casa.compra}`)
             }
+   
+    
     
     })
-        
+escribirHtml()
 })
+
+
+
