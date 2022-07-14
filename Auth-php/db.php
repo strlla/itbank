4bfd39ae31e17cb1606b3feff4e9bd67a13752bf
TomasCalabria <totocalabria.tc@gmail.com>
2022-07-14 18:46:58 -0300

    actualizando develop

--- Auth-php/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $con = mysqli_connect("n1.neatech.ar","admin_itba","aRrcpn2z2f7hdlD5","admin_itba");
-
-    if (mysqli_connect_errno()){
-        echo "Error en la conexion a la base de datos: " . mysqli_connect_error();
-    }
-?>
