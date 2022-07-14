430a99b51df6c29883dc96b8dcc071979c96b16a
TomasCalabria <totocalabria.tc@gmail.com>
2022-07-14 00:15:25 -0300

    Modificando sección 'Clientes'

--- BackEnd/Cliente.py
+++ BackEnd/Cliente.py
@@ -1,11 +1,19 @@
+# Extras:
+# - No se puede crear un cliente sin nombre
+# - No se puede crear un cliente sin dirección
+# - No se puede crear un cliente sin teléfono
+
+
+clientes=[]
 class Cliente:
-    clientes=[]
     def __init__(self,nombre,apellido,numero,dni):
         self.nombre=nombre
         self.apellido=apellido
         self.numero=numero
         self.dni=dni
-              
+        if self.nombre=="" or self.apellido=="" or self.numero=="" or self.dni=="" :
+            raise ValueError("Hay algun campo vacio")
+        
     def __repr__(self):
         return f"Nombre completo: {self.nombre} {self.apellido}\nNum de cuenta: {self.numero}\nDni: {self.dni} "
     def puede_crear_chequera(self):
@@ -14,37 +22,49 @@ class Cliente:
         return bool
     def puede_comprar_dolar(self):
         return bool        
-            
+             
 class cliente_Classic(Cliente):
-    def __init__(self, nombre, apellido, numero, dni,categoria='Classic',tarjeta=bool):
+    def __init__(self, nombre, apellido, numero, dni):
         super().__init__(nombre, apellido, numero, dni)
-        self.__categoria=categoria
-        self.__tarjeta=tarjeta
+        self.__categoria="Classic"
+        clientes.append(self)
     def __repr__(self):
-        return f"Nombre completo: {self.nombre} {self.apellido}\nNum de cuenta: {self.numero}\nDni: {self.dni}\n categoria: {self.__categoria}\n tarjeta: {self.__tarjeta}"
-        
-        def puede_crear_chequera(self):
-            return False
-
-        def puede_comprar_dolar(self):
-            return False
-        
-     
-    
-class cliente_Gold(Cliente):
-    def __init__(self, nombre, apellido, numero, dni, categoria):
-        super().__init__(nombre, apellido, numero, dni, categoria)
+        return f"Nombre completo: {self.nombre} {self.apellido}\nNum de cuenta: {self.numero}\nDni: {self.dni}\nCategoria: {self.__categoria}"
+    def puede_crear_chequera(self):
+        return False
+    def puede_crear_tarjeta(self):
+        return False
     def puede_comprar_dolar(self):
         return False
+ 
     
+class cliente_Gold(Cliente):
+    def __init__(self, nombre, apellido, numero, dni):
+        super().__init__(nombre, apellido, numero, dni)
+        self.__categoria="Gold"
+        clientes.append(self)
+         
+    def __repr__(self):
+        return f"Nombre completo: {self.nombre} {self.apellido}\nNum de cuenta: {self.numero}\nDni: {self.dni}\nCategoria: {self.__categoria}"
     def puede_crear_chequera(self):
-        return 'No'
-
+        return True
+        
 
 class cliente_Black(Cliente):
-    def __init__(self, nombre, apellido, numero, dni, categoria):
-        super().__init__(nombre, apellido, numero, dni, categoria)
+   def __init__(self, nombre, apellido, numero, dni):
+       super().__init__(nombre, apellido, numero, dni)
+       self.__categoria="Black"
+       clientes.append(self)
+       
+   def __repr__(self):
+            return f"Nombre completo: {self.nombre} {self.apellido}\nNum de cuenta: {self.numero}\nDni: {self.dni}\nCategoria: {self.__categoria}"
+   def puede_comprar_dolar(self):
+        return True
+   def puede_crear_chequera(self):
+        return True
+   def puede_crear_tarjeta(self):
+        return True
 
-c=cliente_Classic('Tomas','Calabria','00155687','41562142','Black',True)
-print(c)
-
+a=cliente_Classic("","Perez","123456789","12345678")
+b=cliente_Gold("","Perez","123456789","12345678")
+c=cliente_Black("Mati","Perez","123456789","12345678")
--- BackEnd/Cuenta.py
+++ BackEnd/Cuenta.py
@@ -5,5 +5,6 @@ class Cuenta():
         self.monto = monto
         self.costo_transferencia = costo_transferencia
         self.saldo_descubierto_disponible = saldo_descubierto_disponible
+        
   def __repr__ (self):
         return f"limite_extraccion_diario: {self.limite_extraccion_diario}\nlimite_transferencia_recibida: {self.limite_transferencia_recibida}\nmonto: {self.monto}\ncosto_transferencia: {self.costo_transferencia}\nsaldo descubierto disponible: {self.saldo_descubierto_disponible}" 
