b13bb30a61d4dc5c35f0233b51dcf0f5585f7905
laureanabroccolli <100167774+laureanabroccolli@users.noreply.github.com>
2022-07-14 18:03:56 -0300

    Delete Cliente.s

--- BackEnd/Cliente.s
+++
@@ -1,9 +0,0 @@
-class Cliente ():
-    clientes = []
-def __int__ (self,nombre, apellido, numero, dni, dni, dni):
-self.nombre = nombre 
-self.apellido = apellido
-self.numero = numero
-self.dni = dni
-self.dni = dni
-self.dni = dni
