112c378d4e96b8c9a19d8eb93121e349c0793908
laureanabroccolli <100167774+laureanabroccolli@users.noreply.github.com>
2022-07-14 18:00:24 -0300

    Update Cuenta.py

--- BackEnd/Cuenta.py
+++ BackEnd/Cuenta.py
@@ -6,7 +6,7 @@ class Cuenta():
         self.limite_transferencia_recibida = limite_transferencia_recibida
         self.monto = monto
         self.costo_transferencia = costo_transferencia
-        self.saldo_descubierto_disponible = saldo_descubierto_disponible
+        self.saldo_descubierto_disponible = saldo_descubierto_disponibl
        
   def __repr__ (self):
         return f"limite_extraccion_diario: {self.limite_extraccion_diario}\nlimite_transferencia_recibida: {self.limite_transferencia_recibida}\nmonto: {self.monto}\ncosto_transferencia: {self.costo_transferencia}\nsaldo descubierto disponible: {self.saldo_descubierto_disponible}" 
