226fd42cd2717037af6699dbf4364152b47377bb
laureanabroccolli <100167774+laureanabroccolli@users.noreply.github.com>
2022-07-14 18:03:28 -0300

    Create Cliente.s

---
+++ BackEnd/Cliente.s
@@ -0,0 +1,9 @@
+class Cliente ():
+    clientes = []
+def __int__ (self,nombre, apellido, numero, dni, dni, dni):
+self.nombre = nombre 
+self.apellido = apellido
+self.numero = numero
+self.dni = dni
+self.dni = dni
+self.dni = dni
