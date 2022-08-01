afd1660ab83c9a955d0576690cf6b98cbf586de7
TomasCalabria <totocalabria.tc@gmail.com>
2022-08-01 20:09:42 -0300

    intentando igualar los dueños de las tarjetas a los clientes

--- BackEnd/db/TEST.sql
+++ BackEnd/db/TEST.sql
@@ -1,2 +1,13 @@
-SELECT * from tarjetas
-INNER JOIN cliente on cliente.customer_id=tarjetas.id;
+-- La idea aca va a ser igualar los nombres de los dueños de las tarjetas al de los usuarios
+UPDATE cliente 
+INNER JOIN tarjetas
+ select 
+	tarjetas.Owner,
+	TRIM(substr(tarjetas.Owner, instr(tarjetas.Owner,' ')+2)) AS first_name,
+	TRIM(substr(tarjetas.Owner, 1, instr(tarjetas.Owner,' ')-1)) AS last_name
+from tarjetas
+ON Tarjetas.id=cliente.customer_id
+set first_name = cliente.customer_name, last_name=last_name
+;
+
+
