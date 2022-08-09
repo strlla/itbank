08e3353b9cdfe14d1154d34a038dcb4e34230367
TomasCalabria <totocalabria.tc@gmail.com>
2022-08-09 15:15:03 -0300

    update en views

--- BackEnd/db/querys.sql
+++ BackEnd/db/querys.sql
@@ -10,25 +10,27 @@ SET employee_hire_date = SUBSTR(employee_hire_date, 7, 4) || '-' || SUBSTR(emplo
 ;
 
 -- Renombrar columna y  Calcular edad (No tenemos forma de obtener la fecha del sistema desde vs code)
- 
-UPDATE cliente 
-set customer_age=    "2022/08/05"-cliente.dob
-where cliente.customer_id=cliente.customer_id;
+
+-- alter table cliente add COLUMN customer_age;
+-- UPDATE cliente 
+-- set customer_age=    "2022/08/05"-cliente.dob
+-- where cliente.customer_id=cliente.customer_id;
 
 
 
--- Vista de los clientes clientes MENORES DE 40 por DNI de menor a mayor
-drop VIEW if EXISTS Vista_Clientes;
-CREATE VIEW Vista_Clientes
-AS SELECT cliente.customer_id as DNI,cliente.branch_id AS SucursalID ,cliente.customer_name AS NOMBRE, cliente.customer_age AS EDAD
-FROM cliente
-WHERE customer_age>40 
+-- Vista de los customers  MENORES DE 40 por DNI de menor a mayor
+
+CREATE VIEW Vista_customers as
+SELECT customer_DNI ,branch_id ,customer_name , customer_age 
+FROM cliente 
+WHERE customer_age<40 
 ORDER BY cliente.customer_age ASC;  
 
--- Vista de los clientes clientes tyler y anne por edad de mayor a menor
-drop VIEW if EXISTS Vista_Clientes;
-CREATE VIEW Vista_Clientes
-AS SELECT cliente.customer_id as DNI,cliente.branch_id AS SucursalID ,cliente.customer_name AS NOMBRE , cliente.customer_age AS EDAD
-FROM cliente
-WHERE customer_age>40 AND cliente.customer_name="Anne" or cliente.customer_name="Tyler" 
+-- Vista de los customers  tyler y anne por edad de mayor a menor
+
+
+CREATE VIEW view_clienteName as 
+SELECT branch_id ,customer_name,customer_age 
+FROM cliente 
+WHERE  cliente.customer_name="Anne" or cliente.customer_name="Tyler" 
 ORDER BY cliente.customer_age DESC;  
